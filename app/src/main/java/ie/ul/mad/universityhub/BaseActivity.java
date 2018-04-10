0429e57f36f18c871df11bcb8e6579f567f8a902
NJack1104 <35995458+NJack1104@users.noreply.github.com>
2018-04-10 10:09:36 +0100

    initial commit

---
+++ app/src/main/java/ie/ul/mad/universityhub/BaseActivity.java
@@ -0,0 +1,73 @@
+package ie.ul.mad.universityhub;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+abstract class BaseActivity extends AppCompatActivity {
+    protected FirebaseAuth mAuth;
+    protected FirebaseUser mUser;
+    private static final String TAG = "UniversityHub";
+    protected TextView mDisplayName;
+    protected TextView mEmail;
+    protected TextView mUid;
+
+
+
+    public void checkLogin()
+    {
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        if (mUser == null)
+        {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+    public void signOutUserAccount()
+    {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>()
+                {
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        showPopupMessage("Sign-out completed.");
+                    }
+                });
+        checkLogin();
+        //may need to updateUI
+    }
+
+    public void deleteUserAccount()
+    {
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        showPopupMessage("User account deleted.");
+                    }
+                });
+        signOutUserAccount();
+        //may need to updateUI
+    }
+
+    protected void showPopupMessage(String message)
+    {
+        Log.e(BaseActivity.TAG, message);
+
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
---
+++ app/src/main/java/ie/ul/mad/universityhub/LoginActivity.java
@@ -0,0 +1,98 @@
+package ie.ul.mad.universityhub;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends BaseActivity {
+
+    private static final String TAG = "LoginActivity" ;
+    private static final int RC_SIGN_IN = 1007;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        handleLogin();
+    }
+
+    private void handleLogin()
+    {
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        if (mUser != null)
+        {
+            showPopupMessage("already signed in, " +
+                    "displayName=" + mUser.getDisplayName() + ", " +
+                    "email=" + mUser.getEmail() + ", " +
+                    "uuid=" + mUser.getUid());
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+            );
+            // Create and launch sign-in intent
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK)
+            {
+                mUser = FirebaseAuth.getInstance().getCurrentUser();
+                showPopupMessage("sign in successful, " +
+                        "displayName=" + mUser.getDisplayName() + ", " +
+                        "email=" + mUser.getEmail() + ", " +
+                        "uuid=" + mUser.getUid());
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+            else
+            {
+                if (response == null)
+                {
+                    showPopupMessage("sign in cancelled");
+                    return;
+                }
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK)
+                {
+                    showPopupMessage("no internet connection");
+                    return;
+                }
+                // handle all other errors
+                showPopupMessage("Sign-in error: " + response.getError());
+            }
+        }
+    }
+
+
+}
