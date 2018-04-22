18bf0ee709df8180d790d7f389d3764f68d4a8fc
Trey Balzen <t3balzen@gmail.com>
2018-04-22 19:11:35 +0100

    Added Photo capture to Google Drive for easy info sharing

--- app/src/main/java/ie/ul/mad/universityhub/BaseActivity.java
+++ app/src/main/java/ie/ul/mad/universityhub/BaseActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
